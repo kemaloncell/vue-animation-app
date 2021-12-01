7fb3e56e5250ade3400de782d099c2c183752429
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-12-01 12:35:15 +0300

    Create the component structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
+  <components :is="activeComponent"></components>
 </template>
 <script>
-export default {}
+import GameCards from "./components/GameCards.vue";
+import Failure from "./components/Failure.vue";
+import Celebrate from "./components/Celebrate.vue";
+export default {
+  components: {
+    appGameCards: GameCards,
+    appFailure: Failure,
+    appCelebrate: Celebrate,
+  },
+  data() {
+    return {
+      activeComponent: "app-game-cards",
+    };
+  },
+};
 </script>
 <style></style>
