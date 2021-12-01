e3b71714c6c26a5cadbb73c56c76ca8a26f18b71
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-12-01 13:00:07 +0300

    Create of CSS content and Card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <img src="../assets/card-1.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  width: 200px;
+  height: 232px;
+  border-radius: 5px;
+  border: 1px solid #666;
+  transition: box-shadow 0.5s;
+  margin-left: 15px;
+}
+.card:hover {
+  box-shadow: 0px 5px 48px #666;
+  transition: box-shadow 0.5s;
+}
+.card img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/GameCards.vue
+++ src/components/GameCards.vue
@@ -1,9 +1,41 @@
 <template>
-  <h3>Game card</h3>
+  <div class="game-area">
+    <h1 class="title"><span>Where is the</span> Cat?</h1>
+    <h4 class="description">After choosing one of the open cards, click on the closed card.</h4>
+    <div class="container">
+      <app-card v-for="i in 5" :key="i"></app-card>
+    </div>
+    <div class="container"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Card from "./Card.vue";
+export default {
+  components: {
+    appCard: Card,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.title {
+  text-align: center;
+  color: rosybrown;
+}
+.title span {
+  color: mediumpurple;
+}
+
+.description {
+  color: grey;
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+}
+</style>
